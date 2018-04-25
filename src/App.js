d310b7ed747fc0fce46eae83a28dd246311eb5c5
Kevin <omallek@gmail.com>
2018-04-24 19:21:47 -0500

    adds burger-ingredient component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/layout'
+import BurgerBuilder from './containers/BurgerBuilder/burger-builder'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder />
       </Layout>
       </div>
     );
--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Aux';
+import classes from './layout.css';
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content} >
       {props.children}
     </main>
   </Aux>
