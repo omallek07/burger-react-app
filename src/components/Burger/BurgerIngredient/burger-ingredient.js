cde724b87d4cdcd3424789ef807624996e9ed7fe
Kevin <omallek@gmail.com>
2018-04-26 06:30:55 -0500

    adds transformedIngredients props and obj to array method

--- src/components/Burger/BurgerIngredient/burger-ingredient.js
+++ src/components/Burger/BurgerIngredient/burger-ingredient.js
@@ -9,7 +9,7 @@ class BurgerIngredient extends Component {
       case ('bread-bottom'):
         ingredient = <div className={classes.BreadBottom}></div>
         break;
-      case ('break-top'):
+      case ('bread-top'):
         ingredient = (
           <div className={classes.BreadTop}>
             <div className={classes.Seed1}></div>
