bb26d599c73628a62f107f989c137570b25ca8b6
Kevin <omallek@gmail.com>
2018-04-26 20:05:21 -0500

    creates addMore click handler for ingredients

--- src/containers/BurgerBuilder/burger-builder.js
+++ src/containers/BurgerBuilder/burger-builder.js
@@ -3,21 +3,42 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/burger'
 import BuildControls from '../../components/Burger/BuildControls/build-controls'
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: .8
+}
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
-    }
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  }
+
+  addIngredientHandler = (type) => {
+    let oldCount = this.state.ingredients[type];
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = ++oldCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
   }
 
   render () {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
