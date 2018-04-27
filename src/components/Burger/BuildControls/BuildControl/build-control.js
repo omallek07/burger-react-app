99fb110c523222973793791070d0034904f70d07
Kevin <omallek@gmail.com>
2018-04-26 20:18:39 -0500

    adds disabled logic for ingredients control

--- src/components/Burger/BuildControls/BuildControl/build-control.js
+++ src/components/Burger/BuildControls/BuildControl/build-control.js
@@ -4,8 +4,13 @@ import classes from './build-control.css';
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less}onClick={props.removed}>Less</button>
-    <button className={classes.More} onClick={props.added}>More</button>
+    <button
+      className={classes.Less}
+      disabled={props.disabled}
+      onClick={props.removed}>Less</button>
+    <button
+      className={classes.More}
+      onClick={props.added}>More</button>
   </div>
 )
 
