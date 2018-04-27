856b724ddd04995ee589e3029209bf253067bb0d
Kevin <omallek@gmail.com>
2018-04-26 19:53:06 -0500

    hooks up burger controls to burgerbuilder comp.

--- src/components/Burger/BuildControls/build-controls.js
+++ src/components/Burger/BuildControls/build-controls.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import classes from './build-controls.css'
+import BuildControl from './BuildControl/build-control';
+
+const controls = [
+  { label: 'Salad', type: 'salad'},
+  { label: 'Bacon', type: 'bacon'},
+  { label: 'Cheese', type: 'cheese'},
+  { label: 'Meat', type: 'meat'},
+]
 
 const buildControls = (props) => (
-  <div>
-
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
   </div>
 )
 
-export default buildControls
+export default buildControls;
