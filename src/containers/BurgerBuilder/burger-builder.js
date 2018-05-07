4feee201c1f6ec32e3a814a0b457a194eb3aeef1
Kevin <omallek@gmail.com>
2018-05-07 13:02:28 -0500

    adds click handler for continue and cancel buttons

--- src/containers/BurgerBuilder/burger-builder.js
+++ src/containers/BurgerBuilder/burger-builder.js
@@ -84,28 +84,29 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true })
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Max Swarz',
-        address: {
-          street: '123 test st',
-          zipCode: '34244',
-          country: 'USA'
-        },
-        email: 'test@gmail.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-    .then(response => {
-      this.setState({ loading: false, purchasing: false })
-    })
-    .catch(error => {
-      this.setState({ loading: false, purchasing: false })
-    });
+    // this.setState({ loading: true })
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Max Swarz',
+    //     address: {
+    //       street: '123 test st',
+    //       zipCode: '34244',
+    //       country: 'USA'
+    //     },
+    //     email: 'test@gmail.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    // .then(response => {
+    //   this.setState({ loading: false, purchasing: false })
+    // })
+    // .catch(error => {
+    //   this.setState({ loading: false, purchasing: false })
+    // });
+    this.props.history.push('/checkout');
   }
 
   render () {
