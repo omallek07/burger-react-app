974e6a5e8a49b4d26baa631089565129fa5689aa
Kevin <omallek@gmail.com>
2018-05-07 20:32:29 -0500

    axios to get orders and error handling component for orders

--- src/components/Navigation/NavigationItems/NavigationItem/navigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/navigationItem.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <NavLink to={props.link}>
+    <NavLink
+      to={props.link}
+      exact={props.exact}
+      activeClassName={classes.active}>
       {props.children}
     </NavLink>
   </li>
--- src/components/Navigation/NavigationItems/navigationItems.js
+++ src/components/Navigation/NavigationItems/navigationItems.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import classes from './navigationItems.css'
-import NavigationItem from './NavigationItem/navigationItem'
+import classes from './navigationItems.css';
+import NavigationItem from './NavigationItem/navigationItem';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/">Burger Builder</NavigationItem>
+    <NavigationItem exact link="/">Burger Builder</NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
