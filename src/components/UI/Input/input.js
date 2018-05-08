a6143052c557d4edcfcc76f87bb91b327d92966f
Kevin <omallek@gmail.com>
2018-05-08 13:44:43 -0500

    sets up form validation user error prompts

--- src/components/UI/Input/input.js
+++ src/components/UI/Input/input.js
@@ -3,25 +3,30 @@ import classes from './input.css';
 
 const input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate) {
+    inputClasses.push(classes.Invalid)
+  }
 
   switch(props.elementType) {
     case('input'):
       inputElement = <input
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.changed} />
       break;
     case('textarea'):
       inputElement = <textarea
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.changed} />
       break;
     case('select'):
       inputElement= <select
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         value={props.value}
         onChange={props.changed} >
         {props.elementConfig.options.map(option => (
@@ -33,7 +38,7 @@ const input = (props) => {
         break;
     default:
       inputElement = <input
-        lassName={classes.InputElement}
+        lassName={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.changed} />
--- src/containers/Checkout/ContactData/contact-data.js
+++ src/containers/Checkout/ContactData/contact-data.js
@@ -93,20 +93,17 @@ class ContactData extends Component {
   }
 
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
 
     if (rules.required) {
-      isValid = value.trim() !== '';
+      isValid = value.trim() !== '' && isValid;
     }
-
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
-
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
-
     return isValid;
   }
 
@@ -167,6 +164,8 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
           )
         )}
