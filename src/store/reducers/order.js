f37bb1539f5f66c8ecdb883cb0e13013ca84c737
Kevin <omallek@gmail.com>
2018-05-14 19:48:40 -0500

    creates order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,30 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initalState = {
+  orders: [],
+  loading: false
+};
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_SUCCESS:
+      const newOrder = {
+        ...action.orderData,
+        id: action.orderId
+      };
+      return {
+        ...state,
+        loading: false,
+        orders: state.orders.concat(newOrder)
+      };
+    case actionTypes.FETCH_INGREDIENTS_FAILED:
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
