cbfe59b51bc8a0d1e1e23c439a7425d1c9df3024
Kevin <omallek@gmail.com>
2018-05-20 15:54:32 -0500

    adds error messages to UI for invalid user/passwords

--- src/containers/Auth/auth.js
+++ src/containers/Auth/auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/input';
 import Button from '../../components/UI/Button/button';
+import Spinner from '../../components/UI/Spinner/spinner';
+
 import classes from './auth.css';
 import * as actions from '../../store/actions';
 
@@ -101,7 +103,7 @@ class Auth extends Component {
         });
       }
 
-      const form = formElementsArray.map(formElement => (
+      let form = formElementsArray.map(formElement => (
         <Input
           key={formElement.id}
           elementType={formElement.config.elementType}
@@ -114,8 +116,21 @@ class Auth extends Component {
           changed={(event) => this.inputChangedHandler(event, formElement.id)} />
       ));
 
+      if (this.props.loading) {
+        form = <Spinner />
+      }
+
+      let errorMessage = null;
+
+      if (this.props.error) {
+        errorMessage = (
+          <p>{this.props.error.message}</p>
+        );
+      }
+
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
@@ -130,10 +145,17 @@ class Auth extends Component {
 
 /**** CONTAINER ****/
 
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
   };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -40,8 +40,7 @@ export const auth = (email, password, isSignup) => {
       dispatch(authSuccess(response.data.idToken, response.data.localId));
     })
     .catch(error => {
-      console.log(error);
-      dispatch(authFail(error));
+      dispatch(authFail(error.response.data.error));
     });
   };
 };
