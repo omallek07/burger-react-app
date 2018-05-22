d794b6485cf9026853fbf9ea93e650432f24a38e
Kevin <omallek@gmail.com>
2018-05-22 14:33:16 -0500

    adds one more test to navigationItems component

--- src/components/Navigation/NavigationItems/navigationItems.test.js
+++ src/components/Navigation/NavigationItems/navigationItems.test.js
@@ -7,8 +7,24 @@ import NavigationItem from './NavigationItem/navigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    // method to add props to component
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it('should contain Logout link if authenticated', () => {
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
   });
 });
