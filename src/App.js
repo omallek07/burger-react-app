9613ee5fc5e0e6d049325a89de94f369ec2c7dff
Kevin <omallek@gmail.com>
2018-05-21 19:01:55 -0500

    sets up localstorage for user tokens

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/layout';
 import BurgerBuilder from './containers/BurgerBuilder/burger-builder';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './store/actions';
 import Checkout from './containers/Checkout/checkout';
 import Orders from './containers/Orders/orders';
 import Auth from './containers/Auth/auth';
 import Logout from './containers/Auth/Logout/logout';
 
 class App extends Component {
+
+  componentDidMount () {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
     return (
       <div className="App">
@@ -25,4 +32,12 @@ class App extends Component {
   }
 }
 
-export default App;
+/**** CONTAINER ****/
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -15,6 +15,24 @@ export const authSuccess = (idToken, userId) => {
   };
 };
 
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate > new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+      }
+    }
+  };
+};
+
 export const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
@@ -23,6 +41,9 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -50,6 +71,11 @@ export const auth = (email, password, isSignup) => {
     }
     axios.post(url, authData)
     .then(response => {
+      localStorage.setItem('token', response.data.idToken);
+      const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+      localStorage.setItem('expirationDate', expirationDate);
+      localStorage.setItem('userId', response.data.localId);
+
       dispatch(authSuccess(response.data.idToken, response.data.localId));
       dispatch(checkAuthTimeout(response.data.expiresIn));
     })
