aaf4ce68975f4d2c52edad4eb8dc35920aa9a755
Kevin <omallek@gmail.com>
2018-05-22 14:41:37 -0500

    adds test to burgerBuilder container component

--- src/containers/BurgerBuilder/burger-builder.js
+++ src/containers/BurgerBuilder/burger-builder.js
@@ -11,7 +11,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../store/actions'
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false,
   }
